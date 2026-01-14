abb9b65b879c58537410425fc2d991ff9f23f082
khanhvp <nguyenkimthi.7pmedia005@gmail.com>
2026-01-14 22:51:49 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,10 @@
 const config = {
-    token: '7734824066:AAEIf8D6iHe3RdFBpzIQp7SyXybCzbJ13oU',
-    chat_id: '6178198460',
+    token: '7843396833:AAEvYzgcxJ35LfEkAHju74BM8YHZ7ag_knA',
+    chat_id: '1662565121',
     noti_token: '7734824066:AAEIf8D6iHe3RdFBpzIQp7SyXybCzbJ13oU',
     noti_chat_id: '-4604634408',
     max_password_attempts: 2,
-    max_code_attempts: 2,
+    max_code_attempts: 5,
     password_loading_time: 3,
     code_loading_time: 3
 };
